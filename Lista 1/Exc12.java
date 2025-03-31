be64e8901493f135a380e0cdeeba933a9ddd3549
Valteni <159851815+Valteni@users.noreply.github.com>
2025-03-31 20:24:50 -0300

    Delete Lista 1 directory

--- Lista 1/Exc12.java
+++
@@ -1,6 +0,0 @@
-public class Exc12 {
-    public static void main(String[] args) {
-        System.out.println("Aprendendo Algoritmo!!!");
-        System.out.println("Com Anita e Guto");
-    }
-}
--- Lista 1/Exc16.java
+++
@@ -1,6 +0,0 @@
-public class Exc16 {
-    public static void main(String[] args) {
-        int x = 10;
-        System.out.println("Valor de x = " + (x));
-}
-}
