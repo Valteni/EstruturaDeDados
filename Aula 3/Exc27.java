562a16402abe11ddecf32bab5344e46abbf60e3e
Valteni <159851815+Valteni@users.noreply.github.com>
2025-03-31 20:24:34 -0300

    Add files via upload

---
+++ Aula 3/Exc27.java
@@ -0,0 +1,18 @@
+public class Exc27 {
+    public static void main(String[] args) throws Exception {
+        int x = 2;
+        int y = 5;
+        
+        System.out.print("\nX = " + x);
+        
+        x++;
+        
+        System.out.print("\n\nY = " + y);
+        
+        y--;
+        
+        System.out.print("\n\nNovo valor de x = " + x);
+        System.out.print("\n\nNovo valor de y = " + y);
+        System.out.print("\n\n");
+    }
+}
---
+++ Aula 3/Exc31.java
@@ -0,0 +1,10 @@
+public class Exc31 {
+    public static void main(String[] args) throws Exception {
+        double ma;
+        
+        ma = (8 + 9 + 7) / 3.0;  
+        
+        System.out.println("\nA media aritmetica e: " + ma);
+        System.out.println();  
+    }
+}
